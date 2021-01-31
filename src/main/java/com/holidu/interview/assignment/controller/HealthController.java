a4566f2652bc2cd24c944ca93351db8dab2deea3
meelvin182 <danila@scalable.capital>
2021-01-31 21:22:01 +0100

    typo

--- src/main/java/com/holidu/interview/assignment/controller/HealthController.java
+++ src/main/java/com/holidu/interview/assignment/controller/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-    // Usually I use HealthEndpoint, but no need to add actuator starter for 1 endpoing
+    // Usually I use HealthEndpoint, but no need to add actuator starter for 1 endpoint
     @GetMapping("/")
     public String health() {
         return "Hello world!";
